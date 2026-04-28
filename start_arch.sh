c62174a5cdc4f267a9a49bc00e50c7cb9573acc0
Alice Woodstock <alic3woodstock@gmail.com>
2026-04-28 10:42:44 -0300

    Fix arch linux start script.

--- start_arch.sh
+++ start_arch.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-if [ -e /usr/bin/paru ]; then
+if pacman -Qi python-kivy python-babel python-py7zr python-screeninfo fuse xsel > /dev/null; then
+	echo "Starting..."
+elif [ -e /usr/bin/paru ]; then
 	paru -S python-kivy python-babel python-py7zr python-screeninfo fuse xsel --needed
 elif [ -e /usr/bin/yay ]; then
 	yay -S python-kivy python-babel python-py7zr python-screeninfo fuse xsel --needed
 else
 	sudo pacman -S python-kivy python-babel python-py7zr python-screeninfo fuse xsel --needed
 fi
-
 python main.py
